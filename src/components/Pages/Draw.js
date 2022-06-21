23f9cdcb42be8413568b560387a90125c8da8a4e
0lgatron <perronolga@gmail.com>
2022-06-21 15:21:33 -0400

    improve icons

--- src/components/Pages/Draw.js
+++ src/components/Pages/Draw.js
@@ -6,13 +6,15 @@ import styles from "./Draw.module.css"
 import DownloadButton from '../UI/DownloadButton'
 import DrawButtonContainer from '../UI/DrawButtonContainer'
 
-import Huntress from '../../assets/huntress-icon-removebg-preview.png';
+import Huntress from '../../assets/killers/Huntress.jpg';
 import Freddy from '../../assets/killers/Freddy.jpg'
 import Spirit from '../../assets/killers/Spirit.jpg'
 import Oni from '../../assets/killers/Tony.jpg'
 import Wraith from '../../assets/killers/Wraith.jpg'
 
-import hunt from '../../assets/killers/Huntress.jpg'
+import hunt from '../../assets/icons/hunt_icon.png'
+import fred from '../../assets/icons/fred_icon.png'
+
 
 
 
@@ -26,12 +28,9 @@ const Draw = (props) => {
       <Button onClick={toggleSettings} setting="true"/>
     </ButtonContainer>
     <DrawButtonContainer>
-    <DownloadButton title="Huntress" img={Huntress} drawing={hunt}/>
-    <DownloadButton title="Huntress" img={Huntress} drawing={hunt}/>
+    <DownloadButton title="Huntress" img={hunt} drawing={Huntress}/>
+    <DownloadButton title="Fred" img={fred} drawing={Freddy}/>
 
-    <DownloadButton title="Huntress" img={Huntress} drawing={hunt}/>
-
-    <DownloadButton title="Huntress" img={Huntress} drawing={hunt}/>
 
 
     </DrawButtonContainer>
