6240384cf868345415842bb5cab7e19a11737aee
0lgatron <perronolga@gmail.com>
2023-01-13 16:25:08 -0500

    fix main, add button as it's own ui element

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,27 +1,10 @@
 import React from 'react'
-import {FiSettings} from 'react-icons/fi'
-import {FaPencilAlt} from 'react-icons/fa'
-import {BsMegaphoneFill} from 'react-icons/bs'
 import styles from './Button.module.css'
 
 const Button = (props) => {
-  const { onClick } = props
-  let title;
-  let style;
-  if(props.setting){
-    style = <FiSettings/>
-    title = "Go to Settings"
-  }
-  if(props.draw){
-    style = <FaPencilAlt/>
-    title = "Go to Downloads"
-  } 
-  if (props.main){
-    style = <BsMegaphoneFill/>
-    title = "Go to Categories"
-  }
+  const { onClick, text } = props
   return (
-    <button title={title} className={styles.symbol} onClick={onClick}>{style}</button>
+    <button onClick={onClick} className={styles.button}>{text}</button>
   )
 }
 
