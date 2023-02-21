7d60343dfadf4d0aea7f5ea7087e585bddad2ec3
0lgatron <perronolga@gmail.com>
2023-02-21 16:22:09 -0500

    pub branch

--- src/components/Pages/Main.js
+++ src/components/Pages/Main.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Button from "../UI/Button";
 import styles from "./Main.module.css";
 import { TfiHandPointUp } from "react-icons/tfi";
+import axios from 'axios';
 
 const Main = (props) => {
   const { category, setCategory } = props;
+  useEffect(() => {
+axios.get('https://react-api-3b1e9-default-rtdb.firebaseio.com/movies/-N6iJZLBbL7jZ5Ct6QOW')
+  .then(function (response) {
+    console.log("what i get",response
+    );
+  })
+  }, [category])
+  
   const noCategory = (
     <div className={styles.category__text}>
       <TfiHandPointUp aria-label="finger pointing up" className={styles.icon}/>
